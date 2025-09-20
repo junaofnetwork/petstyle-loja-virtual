deb5b2ad1c6a4e5c5bef6b086c2d4db8bef908e6
João Eduardo <joaobrandpera@gmail.com>
2025-09-19 22:43:18 -0300

    reparo de estruturas do codigo

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Inclui todos os arquivos na pasta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
